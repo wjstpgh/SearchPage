204e757910518964cce5fa977a639fa6e581c964
wjstpgh <sehoJ3756@gmail.com>
2022-08-23 18:06:39 +0900

    로고 제작

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import SearchBar from './searchBar';
 import SearchResult from './searchResult';
+import logo from './galleryLogo.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className="container">
+    <img src={logo} className='logo'></img>
     <SearchBar/>
     <SearchResult/>
   </div>
